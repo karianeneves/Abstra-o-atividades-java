e99d2064fc782c0193b766c1881aa637a265877a
kariane <karianeneves18@gmail.com>
2025-08-04 19:59:40 -0300

    prato.java

--- Abstração atividades/atividades3/prato.java
+++ Abstração atividades/atividades3/prato.java
@@ -2,12 +2,13 @@ public class Prato {
     String nome;
     double preco;
 
-    public Prato(String nome, double preco) {
-        this.nome = nome;
-        this.preco = preco;
+    public Prato(String novonome, double novopreco) {
+        nome = novonome;
+        preco = novopreco;
     }
 
     public void exibirPrato() {
         System.out.printf("Prato: %s, Preço: R$%.2f\n", nome, preco);
     }
+
 }

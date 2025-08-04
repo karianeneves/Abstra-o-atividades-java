7d63808e7e3400a3b291714f13cbaffe71f8f29e
kariane <karianeneves18@gmail.com>
2025-08-04 19:55:06 -0300

    main.java

--- Abstração atividades/atividades1/main.java
+++ Abstração atividades/atividades1/main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Livro meuLivro = new Livro("O Pequeno Príncipe", "Antoine de Saint-Exupéry", "978-85-99296-67-8");
+        Livro meuLivro = new Livro("como fazer amigos e influenciar pessoas", "dale carnegie", "978-85-99296-67-8");
 
         meuLivro.exibirDados();
     }
+
 }

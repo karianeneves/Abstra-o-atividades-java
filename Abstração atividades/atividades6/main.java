336935f4636c9ed10998fba512d787d914a92b9e
kariane <karianeneves18@gmail.com>
2025-08-04 20:03:37 -0300

    main.java

--- Abstração atividades/atividades6/main.java
+++ Abstração atividades/atividades6/main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Curso cursoJava = new Curso("Design 1", "dsg-101", 80);
+        Curso cursoJava = new Curso("Design 1", "dsg01", 80);
 
         cursoJava.exibirDados();
     }
+
 }

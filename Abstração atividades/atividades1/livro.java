4ae0b596cdc0af119ddb27f8cf5740dec7d43add
kariane <karianeneves18@gmail.com>
2025-08-04 19:52:14 -0300

    livro.java

--- Abstração atividades/atividades1/livro.java
+++ Abstração atividades/atividades1/livro.java
@@ -3,10 +3,10 @@ public class Livro {
     String autor;
     String ISBN;
 
-    public Livro(String titulo, String autor, String ISBN) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.ISBN = ISBN;
+    public Livro(String novotitulo, String novoautor, String novoISBN) {
+        titulo = novotitulo;
+        autor = novoautor;
+        ISBN = novoISBN;
     }
 
     public void exibirDados() {
@@ -14,4 +14,5 @@ public class Livro {
         System.out.println("Autor: " + autor);
         System.out.println("ISBN: " + ISBN);
     }
+
 }

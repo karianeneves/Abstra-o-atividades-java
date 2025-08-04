34f36cefbaedc3f522fc4a811a0bcb6a4d0afc5d
kariane <karianeneves18@gmail.com>
2025-08-04 20:01:37 -0300

    Update funcionario.java

--- Abstração atividades/atividades4/funcionario.java
+++ Abstração atividades/atividades4/funcionario.java
@@ -2,12 +2,13 @@ public class Funcionario {
     String nome;
     Departamento departamento;
 
-    public Funcionario(String nome, Departamento departamento) {
-        this.nome = nome;
-        this.departamento = departamento;
+    public Funcionario(String novonome, Departamento novodepartamento) {
+        novonome = nome;
+        novodepartamento = departamento;
     }
 
     public void exibirDados() {
         System.out.println("Funcionário: " + nome + ", Departamento: " + departamento.nome);
     }
+
 }
